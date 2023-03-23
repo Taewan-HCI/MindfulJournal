b8c3a102550cbb9ddecff6e778b8108c57e50050
seolyeongbae <peixueying@gmail.com>
2023-03-23 19:01:21 +0900

    feat:  basic grid system setup, title with contents component in dashboard implement

--- frontend-expert/src/pages/Dashboard.tsx
+++ frontend-expert/src/pages/Dashboard.tsx
@@ -1,7 +1,44 @@
 import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import ContentWithTitle from '../components/ContentWithTitle';
 
 function Dashboard() {
-  return <div>here is dashboard</div>;
+  return (
+    <div>
+      <Container>
+        <Row>
+          <Col xs={4}>
+            <div>
+              나극복 환자의
+              <br />
+              <b>마음챙김 다이어리</b>
+            </div>
+            <ContentWithTitle title="환자 정보">
+              <div>테스트</div>
+            </ContentWithTitle>
+            <ContentWithTitle title="작성 일기 보기">
+              <div>테스트</div>
+            </ContentWithTitle>
+          </Col>
+
+          <Col xs={8}>
+            <ContentWithTitle title="참여 수준">
+              <div>테스트</div>
+            </ContentWithTitle>
+            <ContentWithTitle title="핵심 감정">
+              <div>테스트</div>
+            </ContentWithTitle>
+            <ContentWithTitle title="주요 사건">
+              <div>테스트</div>
+            </ContentWithTitle>
+            <ContentWithTitle title="긍정/부정 비율">
+              <div>테스트</div>
+            </ContentWithTitle>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 }
 
 export default Dashboard;
