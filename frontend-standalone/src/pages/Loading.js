cf806af0ca54cff4d1e7c80e9a232ae00c8f17f5
Taewan Kim <twkim24@gmail.com>
2023-03-23 14:09:56 +0900

    -added standalone mode (running without operator)

---
+++ frontend-standalone/src/pages/Loading.js
@@ -0,0 +1,62 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import {ScaleLoader} from "react-spinners";
+
+function Loading() {
+    return (
+        <div>
+            <Container>
+                <Row>
+                    <Col>
+                        <div className="loading_box">
+                            <div>
+                                <ScaleLoader
+                                    color="#007AFF"
+                                    speedMultiplier={0.9}
+                                />
+                            </div>
+                            <div>지금까지의 이야기를 정리중입니다</div>
+                        </div>
+                    </Col>
+
+                </Row>
+                <Row>
+                    <div className="writing_box">
+                        <Form.Label htmlFor="userInput">✏️ 나의 일기 입력하기</Form.Label>
+                        <Form.Control
+                            type="input"
+                            as="textarea"
+                            rows={3}
+                            id="userInput"
+                            disabled
+                            readOnly
+                        />
+                        <Form.Text id="userInput" muted>
+                            📝 정해진 양식이나 응답이 없어요. 편안하고 자유롭게 최근에 있었던 일을 작성해주세요 .
+                        </Form.Text>
+                    </div>
+                    <Container>
+                        <Row>
+                            <Col>
+                                <div className="d-grid gap-2">
+                                    <Button
+                                        variant="primary"
+                                        disabled={true}
+                                        style={{backgroundColor: "007AFF", fontWeight: "600"}}
+                                    >응답 기록하기</Button>
+                                </div>
+                            </Col>
+                            <Col>
+                            </Col>
+                        </Row>
+                    </Container>
+                </Row>
+            </Container>
+        </div>
+    )
+}
+
+export default Loading
