a244f6395e77a04243d4952a9d595505517bad26
seolyeongbae <peixueying@gmail.com>
2023-03-24 18:48:58 +0900

    feat: patient info block

--- frontend-expert/src/pages/Dashboard.tsx
+++ frontend-expert/src/pages/Dashboard.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
+import { Card, Col, Container, Row } from 'react-bootstrap';
 import ContentWithTitle from '../components/ContentWithTitle';
 import Diary from '../components/Diary';
 import mockDiary from '../mocks/diaryData';
@@ -11,13 +11,24 @@ function Dashboard() {
       <Container>
         <Row>
           <Col xs={4}>
-            <div>
+            <div className="fs-3">
               나극복 환자의
               <br />
               <b>마음챙김 다이어리</b>
             </div>
             <ContentWithTitle title="환자 정보">
-              <div>테스트</div>
+              <Card bg="light" border="light">
+                <Card.Body>
+                  <div className="d-flex align-items-center justify-content-between py-2">
+                    <div className="text-secondary">성별/나이</div>
+                    <div className="fs-6 me-2">남/28</div>
+                  </div>
+                  <div className="d-flex align-items-center justify-content-between py-2">
+                    <div className="text-secondary">최근 진료일</div>
+                    <div className="fs-6 me-2"> 2023.04.03</div>
+                  </div>
+                </Card.Body>
+              </Card>
             </ContentWithTitle>
             <ContentWithTitle title="작성 일기 보기">
               {mockDiary.map((diary) => (
