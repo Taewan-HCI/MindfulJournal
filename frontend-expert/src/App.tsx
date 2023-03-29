378de2b67f488063d70f2ff750210f10b058f7c1
seolyeongbae <peixueying@gmail.com>
2023-03-29 15:15:30 +0900

    refactor: relocation components only realted to dashboard, fix tab layout

--- frontend-expert/src/App.tsx
+++ frontend-expert/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
-import Dashboard from './pages/Dashboard';
+import Dashboard from './pages/dashboard/Dashboard';
 import Main from './pages/Main';
 import PatientsList from './pages/PatientsList';
 
