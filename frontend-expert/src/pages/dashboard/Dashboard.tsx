9d9fd8b9505de5b93af5b26325ec67ce5652434e
seolyeongbae <peixueying@gmail.com>
2023-03-30 17:10:18 +0900

    feat: add change paitient btn, fix layout

--- frontend-expert/src/pages/dashboard/Dashboard.tsx
+++ frontend-expert/src/pages/dashboard/Dashboard.tsx
@@ -11,7 +11,7 @@ import {
   Row,
   ToggleButton,
 } from 'react-bootstrap';
-import { ArrowClockwise } from 'react-bootstrap-icons';
+import { ArrowClockwise, ArrowRepeat } from 'react-bootstrap-icons';
 import ContentWithTitle from '../../components/ContentWithTitle';
 import DateRangePicker from './components/datePicker/DateRangePicker';
 import Diary from './components/Diary';
@@ -32,36 +32,22 @@ function Dashboard() {
   return (
     <div>
       <Container>
-        <Row>
+        <Row className="gx-5">
           <Col xs={4}>
-            <div className="fs-3">
-              나극복 환자의
-              <br />
-              <b>마음챙김 다이어리</b>
+            <div className="d-flex justify-content-between align-items-end mb-4">
+              <div className="fs-2 mt-2">
+                나극복 환자의
+                <br />
+                <b>마음챙김 다이어리</b>
+              </div>
+              <Button variant="info" className="align-self-end ">
+                <ArrowRepeat className="ml-4" />
+                <span className="px-2 fw-bold"> 환자 변경 </span>
+              </Button>
             </div>
-            <ContentWithTitle title="환자 정보">
-              <Card bg="light" border="light">
-                <Card.Body>
-                  <div className="d-flex align-items-center justify-content-between py-2">
-                    <div className="text-secondary">성별/나이</div>
-                    <div className="fs-6 me-2">남/28</div>
-                  </div>
-                  <div className="d-flex align-items-center justify-content-between py-2">
-                    <div className="text-secondary">최근 진료일</div>
-                    <div className="fs-6 me-2"> 2023.04.03</div>
-                  </div>
-                </Card.Body>
-              </Card>
-            </ContentWithTitle>
-            <ContentWithTitle title="작성 일기 보기">
-              {mockDiary.map((diary) => (
-                <Diary key={diary.diaryNum} {...diary} />
-              ))}
-            </ContentWithTitle>
           </Col>
-
-          <Col xs={8}>
-            <Row>
+          <Col xs={8} className="ps-4">
+            <Row className="mt-2">
               <Col xs={3}>
                 <p className="fs-5 fw-bold text-primary">최근 기간 선택</p>
               </Col>
@@ -89,12 +75,12 @@ function Dashboard() {
                   ))}
                 </ButtonGroup>
               </Col>
-            </Row>
-            <Row>
               <Col xs={3}>
-                <p className="fs-5 fw-bold text-primary">분석할 날짜 지정</p>
+                <div className="fs-5 fw-bold text-primary my-auto">
+                  분석할 날짜 지정
+                </div>
               </Col>
-              <Col xs={9} className="d-flex justify-content-between">
+              <Col xs={9} className="d-flex justify-content-between ">
                 <DateRangePicker />
                 <Button variant="primary" className="my-auto">
                   <ArrowClockwise className="ml-4" />
@@ -102,6 +88,31 @@ function Dashboard() {
                 </Button>
               </Col>
             </Row>
+          </Col>
+
+          <Col xs={4} className="border-end">
+            <ContentWithTitle title="환자 정보">
+              <Card bg="light" border="light">
+                <Card.Body>
+                  <div className="d-flex align-items-center justify-content-between py-2">
+                    <div className="text-secondary">성별/나이</div>
+                    <div className="fs-6 me-2">남/28</div>
+                  </div>
+                  <div className="d-flex align-items-center justify-content-between py-2">
+                    <div className="text-secondary">최근 진료일</div>
+                    <div className="fs-6 me-2"> 2023.04.03</div>
+                  </div>
+                </Card.Body>
+              </Card>
+            </ContentWithTitle>
+            <ContentWithTitle title="작성 일기 보기">
+              {mockDiary.map((diary) => (
+                <Diary key={diary.diaryNum} {...diary} />
+              ))}
+            </ContentWithTitle>
+          </Col>
+
+          <Col xs={8}>
             <ContentWithTitle title="참여 수준">
               <Tabs />
             </ContentWithTitle>
