6fbcd434a47cf2506b40d0d527b4e5b0588332fc
seolyeongbae <peixueying@gmail.com>
2023-04-04 20:01:17 +0900

    feat: login form

--- frontend-expert/src/pages/Main.tsx
+++ frontend-expert/src/pages/Main.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Button, Container, Form } from 'react-bootstrap';
 import diary from '../assets/image/diary.png';
 
 function Main() {
   return (
-    <Container>
-      <img className="mx-auto" src={diary} alt="diray img" />
-      <div> 마음챙김 다이어리</div>
+    <Container className="d-flex align-items-center py-2 px-auto flex-column">
+      <img src={diary} alt="diray img" width="60" height="60" />
+      <div className="fs-3"> 마음챙김 다이어리</div>
+
+      <Form className="mt-5">
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>ID</Form.Label>
+          <Form.Control type="email" placeholder="Enter ID" />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Sign In
+        </Button>
+      </Form>
     </Container>
   );
 }
