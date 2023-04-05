2a2b4ad93644dec736a36c931fb97535b9053c84
seolyeongbae <peixueying@gmail.com>
2023-04-05 15:23:11 +0900

    fix: change login form

--- frontend-expert/src/pages/Main.tsx
+++ frontend-expert/src/pages/Main.tsx
@@ -4,11 +4,19 @@ import diary from '../assets/image/diary.png';
 
 function Main() {
   return (
-    <Container className="d-flex align-items-center py-2 px-auto flex-column">
-      <img src={diary} alt="diray img" width="60" height="60" />
-      <div className="fs-3"> 마음챙김 다이어리</div>
-
-      <Form className="mt-5">
+    <Container className="d-flex align-items-center py-2  flex-column">
+      <div className="d-flex align-items-center pt-5 flex-column">
+        <img src={diary} alt="diray img" width="60" height="60" />
+        <div className="fs-2 text-primary mt-2">
+          <b>
+            마음챙김
+            <br />
+            다이어리
+          </b>
+        </div>
+      </div>
+      <Form className="mt-5 w-25">
+        <span className="text-primary mx-auto"> 로그인이 필요합니다. </span>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>ID</Form.Label>
           <Form.Control type="email" placeholder="Enter ID" />
@@ -18,9 +26,12 @@ function Main() {
           <Form.Label>Password</Form.Label>
           <Form.Control type="password" placeholder="Password" />
         </Form.Group>
-        <Button variant="primary" type="submit">
-          Sign In
-        </Button>
+
+        <div className="d-grid gap-1">
+          <Button variant="primary" type="submit">
+            Sign In
+          </Button>
+        </div>
       </Form>
     </Container>
   );
