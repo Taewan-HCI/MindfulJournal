e9d73c110287638f94c716df75399580883f6e18
seolyeongbae <peixueying@gmail.com>
2023-04-18 17:32:44 +0900

    feat: 워드클라우드에 호버하면 횟수 표시되게 수정

--- frontend-expert/src/pages/dashboard/components/CustomWordCloud.tsx
+++ frontend-expert/src/pages/dashboard/components/CustomWordCloud.tsx
@@ -1,37 +1,37 @@
-import React from 'react';
+/* eslint-disable react/jsx-props-no-spreading */
+import React, { useCallback } from 'react';
 import WordCloud from 'react-d3-cloud';
 import { Word } from 'react-d3-cloud/lib/WordCloud';
 import { ResponsiveContainer } from 'recharts';
 
-const data = [
-  { text: '자살', value: 3 },
-  { text: '자해', value: 2 },
-  { text: '생각', value: 2 },
-  { text: '부모님', value: 2 },
-  { text: '고마운', value: 1 },
-  { text: '면도', value: 1 },
-  { text: '그럴', value: 1 },
-  { text: '나', value: 1 },
-  { text: '스스로가', value: 1 },
-  { text: '한심하고', value: 1 },
-  { text: '모습', value: 1 },
-  { text: '남들이', value: 1 },
-  { text: '더더욱', value: 1 },
-  { text: '바보로', value: 1 },
-  { text: '자기들은', value: 1 },
-  { text: '잘났나', value: 1 },
-  { text: '생각도', value: 1 },
-  { text: '들지만', value: 1 },
-  { text: '정작', value: 1 },
-  { text: '사람들', value: 1 },
-  { text: '앞에서면', value: 1 },
-  { text: '한', value: 1 },
-  { text: '마디도', value: 1 },
-  { text: '못하니까', value: 1 },
-];
+type WordCloudData = {
+  text: string;
+  value: number;
+};
 
-function CustomWordCloud() {
+function CustomWordCloud({
+  data,
+  target,
+  setShow,
+  setCountedNum,
+}: {
+  data: WordCloudData[];
+  target: React.MutableRefObject<HTMLDivElement | null>;
+  setShow: React.Dispatch<React.SetStateAction<boolean>>;
+  setCountedNum: React.Dispatch<React.SetStateAction<number>>;
+}) {
   const sensitiveWordsDict = ['자살', '자해']; // 민감한 단어를 추가해서 워드클라우드 표시 할 때 해당 단어들은 붉은 색으로 강조
+
+  const onWordMouseOver = useCallback(
+    (event: any, d: Word) => {
+      // eslint-disable-next-line no-param-reassign
+      target.current = event.target;
+      setShow(() => true);
+      setCountedNum(() => d.value);
+    },
+    [target, setShow, setCountedNum],
+  );
+
   return (
     <ResponsiveContainer width="80%" aspect={4}>
       <WordCloud
@@ -51,18 +51,11 @@ function CustomWordCloud() {
           }
           return '#000000';
         }}
-        onWordClick={(event: any, d: any) => {
-          console.log(`onWordClick: ${d.text}`);
-        }}
-        onWordMouseOver={(event: any, d: any) => {
-          console.log(`onWordMouseOver: ${d.text}`);
-        }}
-        onWordMouseOut={(event: any, d: any) => {
-          console.log(`onWordMouseOut: ${d.text}`);
-        }}
+        onWordMouseOver={onWordMouseOver}
+        onWordMouseOut={() => setShow(() => false)}
       />
     </ResponsiveContainer>
   );
 }
 
-export default CustomWordCloud;
+export default React.memo(CustomWordCloud);
