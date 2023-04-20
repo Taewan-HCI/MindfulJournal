7ef9dcdf433d50d781d3be2f875f7af8fd256e3e
SeolyeongBae <seolyeongbae@gm.gist.ac.kr>
2023-04-20 02:00:57 +0000

    feat: generate NLP apis with gpt and konlp

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,7 @@ import uvicorn
 
 
 if __name__ == "__main__":
-    uvicorn.run("app.gpt3-chat_direct:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("app.gpt3_chat_translate:app", host="0.0.0.0", port=8000, reload=True)
 #
 # if __name__ == "__main__":
 # #     uvicorn.run("app.gpt3_chat:app", host="0.0.0.0", port=8000, reload=True)
