4c57b1985f303860faeb0d4f674ea820a4b82e1e
Taewan Kim <twkim24@gmail.com>
2023-05-30 00:16:43 +0900

    -restAPI -allow feature when token exist

--- backend/app/restAPI.py
+++ backend/app/restAPI.py
@@ -29,8 +29,21 @@ app.add_middleware(
     allow_headers=["*"]
 )
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+# 구글 Firebase인증 및 openAI api key 설정
+load_dotenv()
+gptapi = os.getenv("gptkey")
+cred = credentials.Certificate(
+    '/Users/taewankim/PycharmProjects/LLM_diary/backend/mindfuljournal-44166-firebase-adminsdk-frpg8-10b50844cf.json')
+app_1 = firebase_admin.initialize_app(cred)
+db = firestore.client()
+My_OpenAI_key = gptapi
+openai.api_key = My_OpenAI_key
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/token")
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+
 
 class TokenData(BaseModel):
     username: Optional[str] = None
@@ -68,17 +81,37 @@ class User(BaseModel):
     hashed_password: str
 
 
+class UserBase(BaseModel):
+    username: str
+
+class UserIn(UserBase):
+    hashed_password: str
+
+class User(UserBase):
+    hashed_password: Optional[str] = None
+
+
 def get_user(fake_db, username: str):
     if username in fake_db:
         user_dict = fake_db[username]
         return User(**user_dict)
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/token")
+
+def decode_access_token(token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    return token_data
 
 async def get_current_user(token: str = Depends(oauth2_scheme)):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials. Sorry",
+        detail="Could not validate credentials",
         headers={"WWW-Authenticate": "Bearer"},
     )
     try:
@@ -89,10 +122,21 @@ async def get_current_user(token: str = Depends(oauth2_scheme)):
         token_data = TokenData(username=username)
     except JWTError:
         raise credentials_exception
-    user = get_user(fake_db=fake_db, username=token_data.username)
+
+    user = fake_db.get(token_data.username)
     if user is None:
         raise credentials_exception
-    return user
+    return User(username=token_data.username)
+
+def download():
+    doc_ref = db.collection(u'session').document("ut01@test.com").collection(u'diary').document("G02")
+    doc = doc_ref.get()
+    if doc.exists:
+        print(f'Document data: {doc.to_dict()}')
+        return doc.to_dict()
+    else:
+        print(u'No such document!')
+
 
 fake_db = {
     "expertReview": {
@@ -116,8 +160,11 @@ def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
     )
     return {"access_token": access_token, "token_type": "bearer"}
 
+@app.get("/test")
+async def test_endpoint(current_user: User = Depends(get_current_user)):
+    return {"message": "authorized"}
 
-
-@app.get("/users/me/")
-async def read_users_me(current_user: User = Depends(get_current_user)):
-    return current_user
+@app.get("/test1")
+async def read_root(current_user: User = Depends(get_current_user)) -> dict:
+    response = download()
+    return response
