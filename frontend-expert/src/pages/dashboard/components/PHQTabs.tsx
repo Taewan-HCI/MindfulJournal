9ad843cd04b326162bba64073ce6b51d1c689946
seolyeongbae <peixueying@gmail.com>
2023-06-16 15:28:38 +0900

    feat: table 생성

--- frontend-expert/src/pages/dashboard/components/PHQTabs.tsx
+++ frontend-expert/src/pages/dashboard/components/PHQTabs.tsx
@@ -1,59 +1,27 @@
-import React, { useState } from 'react';
-import { Card, Nav } from 'react-bootstrap';
-import { PHQ_QUESTIONS, PHQ_RESPONSE } from 'constants/phq';
-
-function TabContent({ tab, data }: { tab: string | null; data: number[] }) {
-  if (tab === null) {
-    return (
-      <>
-        <Card.Title>카드 제목</Card.Title>
-        <Card.Text>please select tab</Card.Text>
-      </>
-    );
-  }
-
-  const filteredIndex = data.reduce((arr: number[], item, index) => {
-    if (`${item}` === tab) {
-      arr.push(index);
-    }
-    return arr;
-  }, []);
-
-  return (
-    <Card.Text>
-      {filteredIndex.map((i) => (
-        <div>{PHQ_QUESTIONS[i]}</div>
-      ))}
-    </Card.Text>
-  );
-}
+/* eslint-disable react/no-array-index-key */
+import { PHQ_QUESTIONS } from 'constants/phq';
+import React from 'react';
+import { Table } from 'react-bootstrap';
 
 function PHQTabs() {
-  const [key, setKey] = useState<string | null>('0');
-  const keys = Object.keys(PHQ_RESPONSE);
-
+  const data = [1, 1, 3, 2, 1, 1, 1, 1, 1];
   return (
-    <Card>
-      <Card.Header>
-        <Nav
-          variant="tabs"
-          activeKey={key ?? '0'}
-          onSelect={(k) => setKey(k)}
-          fill
-        >
-          {keys.map((k) => (
-            <Nav.Item key={k}>
-              <Nav.Link eventKey={k}>
-                <span>{PHQ_RESPONSE[parseInt(k as string, 10)]} </span>
-              </Nav.Link>
-            </Nav.Item>
-          ))}
-        </Nav>
-      </Card.Header>
-      <Card.Body>
-        <TabContent tab={key} data={[0, 0, 0, 0, 0, 1, 2, 2, 3]} />
-      </Card.Body>
-    </Card>
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>Question</th>
+          <th>Score</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((score, index) => (
+          <tr key={`${score}-${index}`}>
+            <td>{PHQ_QUESTIONS[index]}</td>
+            <td>{score}</td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
   );
 }
 
